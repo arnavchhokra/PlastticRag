c5c7a1c2f1ec50c16d79825b16a074a0bf831b17
Arnav Chhokra <arnavchhokra@gmail.com>
2024-10-15 11:58:17 +0530

    Commit

--- client/src/components/Carousel/CarouselComponent.tsx
+++ client/src/components/Carousel/CarouselComponent.tsx
@@ -8,16 +8,16 @@ import {
     CarouselItem,
 } from "@/components/ui/carousel";
 import Image from "next/image";
-import image1 from '@/public/1.webp';
-import image2 from '@/public/2.webp';
-import image3 from '@/public/3.webp';
-import image4 from '@/public/4.webp';
-import image5 from '@/public/5.webp';
-import image6 from '@/public/6.webp';
-import image7 from '@/public/7.webp';
-import image8 from '@/public/8.webp';
-import image9 from '@/public/9.webp';
-import image10 from '@/public/10.webp';
+import image1 from '../../../public/1.webp';
+import image2 from '../../../public/2.webp';
+import image3 from '../../../public/3.webp';
+import image4 from '../../../public/4.webp';
+import image5 from '../../../public/5.webp';
+import image6 from '../../../public/6.webp';
+import image7 from '../../../public/7.webp';
+import image8 from '../../../public/8.webp';
+import image9 from '../../../public/9.webp';
+import image10 from '../../../public/10.webp';
 
 const images = [
     image1,
